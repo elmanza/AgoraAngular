cf91d61dd6d4978a616703004dfc42fe3d72514e
Andres Manzano <andres.manzano@empowermentlabs.co>
2023-08-13 18:45:22 -0500

    Feature: New pages was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HostComponent } from './pages/host/host.component';
+import { SearchComponent } from './pages/search/search.component';
+import { RoomComponent } from './pages/room/room.component';
+import { HelpComponent } from './pages/help/help.component';
+import { BookComponent } from './pages/book/book.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { WishlistsComponent } from './pages/wishlists/wishlists.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { TripsComponent } from './pages/trips/trips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    HomeComponent,
+    HostComponent,
+    SearchComponent,
+    RoomComponent,
+    HelpComponent,
+    BookComponent,
+    NotificationsComponent,
+    WishlistsComponent,
+    ChatComponent,
+    TripsComponent
   ],
   imports: [
     BrowserModule
