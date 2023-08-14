43c8660968476729f2287f0ac7eca1c34a71f583
Andres Manzano <andres.manzano@empowermentlabs.co>
2023-08-13 23:53:25 -0500

    Best practices integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardsComponent } from './components/cards/cards.component';
-import { HomeComponent } from './pages/home/home.component';
-import { HostComponent } from './pages/host/host.component';
-import { SearchComponent } from './pages/search/search.component';
-import { RoomComponent } from './pages/room/room.component';
-import { HelpComponent } from './pages/help/help.component';
-import { BookComponent } from './pages/book/book.component';
-import { NotificationsComponent } from './pages/notifications/notifications.component';
-import { WishlistsComponent } from './pages/wishlists/wishlists.component';
-import { ChatComponent } from './pages/chat/chat.component';
-import { TripsComponent } from './pages/trips/trips.component';
+import { HomeComponent } from './pages/public/home/home.component';
+import { HostComponent } from './pages/public/host/host.component';
+import { SearchComponent } from './pages/public/search/search.component';
+import { RoomComponent } from './pages/public/room/room.component';
+import { HelpComponent } from './pages/public/help/help.component';
+import { BookComponent } from './pages/public/book/book.component';
+import { NotificationsComponent } from './pages/private/notifications/notifications.component';
+import { WishlistsComponent } from './pages/private/wishlists/wishlists.component';
+import { ChatComponent } from './pages/private/chat/chat.component';
+import { TripsComponent } from './pages/private/trips/trips.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'room/:property_id',
+    component: RoomComponent
+  },
+  {
+    path: 'book/:property_id',
+    component: BookComponent
+  },
+  {
+    path: 'host',
+    component: HostComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   declarations: [
@@ -27,10 +62,12 @@ import { TripsComponent } from './pages/trips/trips.component';
     NotificationsComponent,
     WishlistsComponent,
     ChatComponent,
-    TripsComponent
+    TripsComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/public/room/room.component.ts
+++ src/app/pages/public/room/room.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent {
-
+  constructor(private route: ActivatedRoute){
+    this.route.params.subscribe(params => console.log(params));
+  }
 }
